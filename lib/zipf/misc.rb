51e19d21496ec977843f1c10c229c5370cd381b4
Patrick Simianer <p@simianer.de>
2015-01-25 13:27:20 +0100

    Integer::MAX Integer::MIN

--- lib/zipf/misc.rb
+++ lib/zipf/misc.rb
@@ -111,3 +111,11 @@ def read_config fn
   return cfg
 end
 
+# https://gist.github.com/pithyless/9738125
+class Integer
+  N_BYTES = [42].pack('i').size
+  N_BITS = N_BYTES * 16
+  MAX = 2 ** (N_BITS - 2) - 1
+  MIN = -MAX - 1
+end
+
